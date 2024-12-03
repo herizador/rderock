94ef3a4b34e69176d739be02af1b466d3c3e7f65
herizador <155181668+herizador@users.noreply.github.com>
2024-12-03 19:30:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.post('/subscribe', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
